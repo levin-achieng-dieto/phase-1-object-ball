b37d8131f5ca4d3b4d04be2f384fd5afef5cca55
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-22 13:26:22 +0300

    Completed assignment

---
+++ src/00-objectball.js
@@ -0,0 +1,36 @@
+const gameObject = {
+    home: {
+        teamName: "Brooklyn Nets",
+        colors: ["Black", "White"],
+        players: {
+            AlanAnderson: {
+                number: "0",
+                shoe: "16",
+                points: "22",
+                rebounds: "12",
+                assists: "12",
+                steals: "3",
+                blocks: "1",
+                slamDunks: "1"
+        },
+        }
+    },
+    away: {
+        teamName: "Charlotte Hornets",
+        colors: ["Turquoise", "Purple"],
+        players: {
+            jeffAdrien: {
+                number: "4",
+                shoe: "18",
+                points: "10",
+                rebounds: "1",
+                assists: "1",
+                steals: "2",
+                blocks: "7",
+                slamDunks: "2"
+            }
+        }
+    }
+}
+
+console.log(gameObject);
